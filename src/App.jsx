07016734a426108ad7ad9a94b951b26e7c66b42d
NataMarques08 <nmolinamarques@gmail.com>
2025-05-29 15:26:12 -0300

    Creating display component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Display from './components/display/Displey';
+
+
+function App(){
+  return (
+    <div className="app">
+        <Display/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/display/Displey.jsx
@@ -0,0 +1,10 @@
+import './Display.css';
+
+function Display(){
+    return (
+        <div className="display"> 
+            <h1>To Do List</h1>
+        </div>
+    )
+}
+export default Display;
